93f3340ff116125c47fa22712a020f5c13c57493
Antonio-Gabriel <antoniocamposgabriel@gmail.com>
2022-04-13 22:47:47 +0100

    create a models

--- src/app/sql/models.py
+++ src/app/sql/models.py
@@ -1,87 +1,69 @@
-import psycopg2
 from datetime import datetime
+from sqlalchemy import Column, Boolean, String, TIMESTAMP, ForeignKey, DateTime
+from sqlalchemy.dialects.postgresql import ENUM
+from sqlalchemy.orm import declarative_base
 
 from .enums.Users import Users
 
-from sqlalchemy import (
-    Table,
-    Enum,
-    Column,
-    String,
-    Boolean,
-    MetaData,
-    DateTime,
-    ForeignKey,
-    TIMESTAMP,
-    create_engine,
-)
-
-engine = create_engine(
-    "postgresql://admin:postgresdb2022@localhost/courseDb", echo=True
-)
-metadata_object = MetaData(bind=engine)
-
-user = Table(
-    "user",
-    metadata_object,
-    Column("id", String(40), nullable=False, primary_key=True),
-    Column("password", String(60), nullable=False),
-    Column("name", String(60), nullable=False),
-    Column("surname", String(60), nullable=False),
-    Column("email", String(60), nullable=False),
-    Column("state", Boolean, default=True),
-    Column("city", String(45), nullable=False),
-    Column("address", String(45), nullable=False),
-    Column("type", Enum(Users), default="staff"),
-)
-
-teacher = Table(
-    "teacher",
-    metadata_object,
-    Column("id", String(40), nullable=False, primary_key=True),
-    Column("user_id", String(40), ForeignKey("user.id")),
-    Column("created_at", TIMESTAMP, default=datetime.now),
-    Column("updated_at", TIMESTAMP, default=datetime.now, onupdate=datetime.now),
-)
-
-student = Table(
-    "student",
-    metadata_object,
-    Column("id", String(40), nullable=False, primary_key=True),
-    Column("user_id", String(40), ForeignKey("user.id")),
-    Column("created_at", TIMESTAMP, default=datetime.now),
-    Column("updated_at", TIMESTAMP, default=datetime.now, onupdate=datetime.now),
-)
-
-course = Table(
-    "course",
-    metadata_object,
-    Column("id", String(40), nullable=False, primary_key=True),
-    Column("name", String(60), nullable=False),
-    Column("startDate", DateTime, nullable=False),
-    Column("endDate", DateTime, nullable=False),
-    Column("state", Boolean, default=True),
-    Column("teacher_id", String(40), ForeignKey("teacher.id")),
-    Column("created_at", TIMESTAMP, default=datetime.now),
-    Column("updated_at", TIMESTAMP, default=datetime.now, onupdate=datetime.now),
-)
-
-enrollment = Table(
-    "enrollment",
-    metadata_object,
-    Column("student_id", String(40), ForeignKey("student.id")),
-    Column("course_id", String(40), ForeignKey("course.id")),
-    Column("created_at", TIMESTAMP, default=datetime.now),
-    Column("updated_at", TIMESTAMP, default=datetime.now, onupdate=datetime.now),
-)
-
-contact = Table(
-    "contact",
-    metadata_object,
-    Column("id", String(40), nullable=False, primary_key=True),
-    Column("phone", String(40), nullable=False, primary_key=True),
-    Column("user_id", String(40), ForeignKey("user.id")),
-)
+Base = declarative_base()
 
 
-metadata_object.create_all()
+class UserModel(Base):
+    __tablename__ = "tusers"
+
+    id = Column(String(40), primary_key=True)
+    password = Column(String(60), nullable=False)
+    name = Column(String(60), nullable=False)
+    surname = Column(String(60), nullable=False)
+    email = Column(String(60), nullable=False)
+    state = Column(Boolean, default=True)
+    city = Column(String(45), nullable=False)
+    user_type = Column(ENUM(Users), nullable=False)
+
+
+class TeacherModel(Base):
+    __tablename__ = "teachers"
+
+    id = Column(String(40), nullable=False, primary_key=True)
+    user_id = Column(String(40), ForeignKey("tusers.id"))
+    created_at = Column(TIMESTAMP, default=datetime.utcnow)
+    updated_at = Column(TIMESTAMP, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+
+class StudentModel(Base):
+    __tablename__ = "students"
+
+    id = Column(String(40), nullable=False, primary_key=True)
+    user_id = Column(String(40), ForeignKey("tusers.id"))
+    created_at = Column(TIMESTAMP, default=datetime.utcnow)
+    updated_at = Column(TIMESTAMP, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+
+class CourseModel(Base):
+    __tablename__ = "courses"
+
+    id = Column(String(40), nullable=False, primary_key=True)
+    name = Column(String(60), nullable=False)
+    startDate = Column(DateTime, nullable=False)
+    endDate = Column(DateTime, nullable=False)
+    state = Column(Boolean, default=True)
+    teacher_id = Column(String(40), ForeignKey("teachers.id"))
+    created_at = Column(TIMESTAMP, default=datetime.utcnow)
+    updated_at = Column(TIMESTAMP, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+
+class EnrollmentModel(Base):
+    __tablename__ = "enrollments"
+
+    student_id = Column(String(40), ForeignKey("students.id"), primary_key=True)
+    course_id = Column(String(40), ForeignKey("courses.id"), primary_key=True)
+    created_at = Column(TIMESTAMP, default=datetime.utcnow)
+    updated_at = Column(TIMESTAMP, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+
+class ContactModel(Base):
+    __tablename__ = "contacts"
+
+    id = Column(String(40), nullable=False, primary_key=True)
+    phone = Column(String(40), nullable=False, primary_key=True)
+    user_id = Column(String(40), ForeignKey("tusers.id"))
