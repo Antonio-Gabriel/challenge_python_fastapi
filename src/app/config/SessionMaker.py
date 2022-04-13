de27f81aeac4b272a7ad0fc901ba8616291f2afb
Antonio-Gabriel <antoniocamposgabriel@gmail.com>
2022-04-14 00:51:27 +0100

    config session maker

---
+++ src/app/config/SessionMaker.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+session_maker = sessionmaker(
+    bind=create_engine(
+        "postgresql://postgres:postgresdb2022@localhost:5432/wedevLearning"
+    )
+)
