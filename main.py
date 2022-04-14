0c8f8dec0b87e84f0e4e96f9cc3bd4b2061613bf
Antonio-Gabriel <antoniocamposgabriel@gmail.com>
2022-04-14 17:55:06 +0100

    configure server

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
-# from sqlalchemy import (
-#     create_engine,
-#     databases
-# )
-
-# from sqlalchemy.databases import postgresql
-
+import uvicorn
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+from src.app.routes.user.UserRoutes import user_routes
+
+app = FastAPI(redoc_url=False)
+
+app.title = "WeDev Learning"
+app.description = "Documentation of wedev learning"
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="*",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-@app.get("/")
+@app.get("/", tags=["About"])
 def say_hello():
     """Greeting"""
-    return {"msg": "Hello World"}
+    return {"msg": "The test of wedev software company"}
+
+
+app.include_router(user_routes)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
