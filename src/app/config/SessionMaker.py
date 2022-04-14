144bb4463f2ae7b66397b15ef79616faaac8dc81
Antonio-Gabriel <antoniocamposgabriel@gmail.com>
2022-04-14 17:54:24 +0100

    update session marker configuration

--- src/app/config/SessionMaker.py
+++ src/app/config/SessionMaker.py
@@ -4,5 +4,5 @@ from sqlalchemy.orm import sessionmaker
 session_maker = sessionmaker(
     bind=create_engine(
         "postgresql://postgres:postgresdb2022@localhost:5432/wedevLearning"
-    )
+    ),
 )
