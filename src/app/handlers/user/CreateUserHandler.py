315cc31ee2539ee6d860700ec7eddc95047dcfbf
Antonio-Gabriel <antoniocamposgabriel@gmail.com>
2022-04-14 17:55:18 +0100

    create user handlers

---
+++ src/app/handlers/user/CreateUserHandler.py
@@ -0,0 +1,24 @@
+from typing import Type
+
+from ...entities.props import UserProps
+from ...interfaces import IUserRepository
+
+from src.shared.src.logic import Result
+from .EmailAlreadyExists import EmailAlreadyExists
+
+
+class CreateUserHandler:
+    def __init__(self, user_repository: Type[IUserRepository]) -> None:
+        self.__user_repository = user_repository
+
+    def handle(self, requestDTO: UserProps) -> Result[UserProps]:
+
+        email_already_exists = self.__user_repository.get_by_email(requestDTO.email)
+
+        if email_already_exists:
+            return EmailAlreadyExists(requestDTO.email)
+
+        statement = self.__user_repository.save(requestDTO)
+
+        if statement:
+            return Result.ok(UserProps)
