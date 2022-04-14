46886d4904ab167517a73b6390aa0a0a44d9fcf2
Antonio-Gabriel <antoniocamposgabriel@gmail.com>
2022-04-14 17:56:13 +0100

    get user by state

---
+++ src/app/handlers/user/GetUserByStateHandler.py
@@ -0,0 +1,16 @@
+from typing import Type
+
+from ...interfaces import IUserRepository
+
+from src.shared.src.logic import Result
+
+
+class GetUserByStateHandler:
+    def __init__(self, user_repository: Type[IUserRepository]) -> None:
+        self.__user_repository = user_repository
+
+    def handle(self, user_state: str) -> Result[dict]:
+
+        filtered_user = self.__user_repository.get_by_state(user_state)
+
+        return Result.ok(filtered_user)
