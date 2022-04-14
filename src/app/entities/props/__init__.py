598204b305b879a8204ce912fb880eb473b8dc21
Antonio-Gabriel <antoniocamposgabriel@gmail.com>
2022-04-14 01:33:24 +0100

    module

--- src/app/entities/props/__init__.py
+++ src/app/entities/props/__init__.py
@@ -1,1 +1,6 @@
 from .UserProps import UserProps
+from .CourseProps import CourseProps
+from .StudentProps import StudentProps
+from .ContactProps import ContactProps
+from .TeacherProps import TeacherProps
+from .EnrollmentProps import EnrollmentProps
