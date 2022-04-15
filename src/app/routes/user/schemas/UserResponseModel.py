0716635b128c0e3582ba125c2980b47107c04e44
Antonio-Gabriel <antoniocamposgabriel@gmail.com>
2022-04-15 12:33:46 +0100

    user routes schemas

---
+++ src/app/routes/user/schemas/UserResponseModel.py
@@ -0,0 +1,43 @@
+from enum import Enum
+from typing import List
+from pydantic import BaseModel
+
+
+class UserModels(BaseModel):
+    id: str
+    name: str
+    email: str
+    surname: str
+    city: str
+    password: str
+    state: bool
+    user_type: Enum
+
+    class Config:
+        orm_mode = True
+
+
+class UserCreateModels(BaseModel):
+    name: str
+    email: str
+    surname: str
+    city: str
+    password: str
+    state: bool
+
+    class Config:
+        orm_mode = True
+
+
+class UserResponseModel(BaseModel):
+    users: List[UserModels]
+
+    class Config:
+        orm_mode = True
+
+
+class UserByIdResponseModel(BaseModel):
+    user: UserModels
+
+    class Config:
+        orm_mode = True
