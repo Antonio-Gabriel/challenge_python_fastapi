752ff8285aa58d81e15ebf4666adc378f0f8eab1
Antonio-Gabriel <antoniocamposgabriel@gmail.com>
2022-04-15 12:31:42 +0100

    modules

---
+++ src/app/adapter/__init__.py
@@ -0,0 +1,1 @@
+from .UserAdapter import UserAdapter
--- src/app/handlers/__init__.py
+++ src/app/handlers/__init__.py
@@ -3,3 +3,5 @@ from .user.DeleteUserHandler import DeleteUserHandler
 from .user.UpdateUserHandler import UpdateUserHandler
 from .user.GetAllUserHandlers import GetAllUserHandlers
 from .user.GetUsersByIdHandler import GetUsersByIdHandler
+from .user.GetUserByNameHandler import GetUserByNameHandler
+from .user.GetUserByStateHandler import GetUserByStateHandler
