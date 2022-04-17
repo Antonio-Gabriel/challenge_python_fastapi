8e8c0a258e56a325f3a50ac245611e8fd35a1ae7
Antonio-Gabriel <antoniocamposgabriel@gmail.com>
2022-04-17 16:25:00 +0100

    user routes

--- src/app/routes/user/UserRoutes.py
+++ src/app/routes/user/UserRoutes.py
@@ -1,20 +1,23 @@
 from fastapi import APIRouter, status, Depends
 
-from ...adapter import UserAdapter
 from ...config import AuthMiddleware
-from ...repositories import UserRepository
+from ...adapter import UserAdapter, TeacherAdapter
 from ...decorators.Authorization import Authorization
+from ...repositories import UserRepository, TeacherRepository
 
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 
 from .schemas.UserResponseModel import (
+    GenericUserModel,
     UserCreateModels,
     UserResponseModel,
     UserByIdResponseModel,
 )
 
+from .schemas.TeacherSchema import TeacherSchema
 from .schemas.AuthSchema import AuthSchema, AuthResponseModel
+from .schemas.TeacherResponseModel import TeacherResponseModel
 
 from ...handlers import (
     AuthUserHandler,
@@ -25,6 +28,8 @@ from ...handlers import (
     GetUsersByIdHandler,
     GetUserByNameHandler,
     GetUserByStateHandler,
+    CreateTeacherHandler,
+    GetAllTeachersHandler,
 )
 
 user_routes = APIRouter(
@@ -62,7 +67,7 @@ async def auth(credentials: AuthSchema):
     }
 
 
-@user_routes.get("/", response_model=UserResponseModel)
+@user_routes.get("/users", response_model=UserResponseModel)
 @Authorization("staff")
 async def get(auth=Depends(AuthMiddleware.auth_wrapper)):
     """Get all users"""
@@ -72,7 +77,7 @@ async def get(auth=Depends(AuthMiddleware.auth_wrapper)):
     return {"users": result.get_value()}
 
 
-@user_routes.get("/search/", response_model=UserResponseModel)
+@user_routes.get("/users/search/", response_model=UserResponseModel)
 @Authorization("staff")
 async def get_by_name(name: str, auth=Depends(AuthMiddleware.auth_wrapper)):
     """Get user by name"""
@@ -82,7 +87,7 @@ async def get_by_name(name: str, auth=Depends(AuthMiddleware.auth_wrapper)):
     return {"users": results.get_value()}
 
 
-@user_routes.get("/actives", response_model=UserResponseModel)
+@user_routes.get("/users/actives", response_model=UserResponseModel)
 @Authorization("staff")
 async def get_by_actives(auth=Depends(AuthMiddleware.auth_wrapper)):
     """Get all active users"""
@@ -92,7 +97,7 @@ async def get_by_actives(auth=Depends(AuthMiddleware.auth_wrapper)):
     return {"users": results.get_value()}
 
 
-@user_routes.get("/desactives", response_model=UserResponseModel)
+@user_routes.get("/users/desactives", response_model=UserResponseModel)
 @Authorization("staff")
 async def get_by_desactive(auth=Depends(AuthMiddleware.auth_wrapper)):
     """Get all desactive users"""
@@ -102,7 +107,7 @@ async def get_by_desactive(auth=Depends(AuthMiddleware.auth_wrapper)):
     return {"users": results.get_value()}
 
 
-@user_routes.get("/{id}", response_model=UserByIdResponseModel)
+@user_routes.get("/user/{id}", response_model=UserByIdResponseModel)
 @Authorization("staff")
 async def get_by_id(id: str, auth=Depends(AuthMiddleware.auth_wrapper)):
     """Get user by id"""
@@ -122,7 +127,11 @@ async def get_by_id(id: str, auth=Depends(AuthMiddleware.auth_wrapper)):
     )
 
 
-@user_routes.post("/create")
+@user_routes.post(
+    "/user/create",
+    response_model=GenericUserModel,
+    response_description="Success Responde",
+)
 async def create(user_props: UserCreateModels):
     """Create user staff"""
 
@@ -172,7 +181,11 @@ async def create(user_props: UserCreateModels):
     )
 
 
-@user_routes.put("/update/{id}")
+@user_routes.put(
+    "/user/update/{id}",
+    response_model=GenericUserModel,
+    response_description="Success Responde",
+)
 @Authorization("staff")
 async def update(
     id: str, user_props: UserCreateModels, auth=Depends(AuthMiddleware.auth_wrapper)
@@ -226,7 +239,11 @@ async def update(
     )
 
 
-@user_routes.delete("/delete/{id}")
+@user_routes.delete(
+    "/user/delete/{id}",
+    response_model=GenericUserModel,
+    response_description="Success Responde",
+)
 @Authorization("staff")
 async def delete(id: str, auth=Depends(AuthMiddleware.auth_wrapper)):
     """Delete user staff"""
@@ -254,3 +271,74 @@ async def delete(id: str, auth=Depends(AuthMiddleware.auth_wrapper)):
             }
         ),
     )
+
+
+# Section for teacher create
+@user_routes.post(
+    "/teacher/create",
+    response_model=GenericUserModel,
+    response_description="Success Responde",
+)
+@Authorization("staff")
+async def create_teacher(
+    user_props: TeacherSchema, auth=Depends(AuthMiddleware.auth_wrapper)
+):
+    """Create user staff"""
+
+    user_result = UserAdapter.create(
+        name=user_props.name,
+        email=user_props.email,
+        surname=user_props.surname,
+        password=user_props.password,
+        city=user_props.city,
+        state=user_props.state,
+        user_type="teacher",
+    )
+
+    error_entity = user_result.error_value()
+    if error_entity:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content=jsonable_encoder(
+                {
+                    "detail": {"msg": error_entity},
+                }
+            ),
+        )
+
+    teacher_result = TeacherAdapter.create(user_id=user_result.get_value().id)
+
+    teacher_handler = CreateTeacherHandler(UserRepository, TeacherRepository)
+    result = teacher_handler.handle(
+        user_result.get_value(), teacher_result.get_value(), user_props.contacts
+    )
+
+    error = result.error_value()
+
+    if error:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content=jsonable_encoder(
+                {
+                    "detail": {"msg": error},
+                }
+            ),
+        )
+
+    return JSONResponse(
+        status_code=status.HTTP_201_CREATED,
+        content=jsonable_encoder(
+            {
+                "detail": {"msg": "Successfully teacher created!"},
+            }
+        ),
+    )
+
+
+@user_routes.get("/teachers", response_model=TeacherResponseModel)
+@Authorization("staff")
+async def get_teachers(auth=Depends(AuthMiddleware.auth_wrapper)):
+
+    teacher_handle = GetAllTeachersHandler(TeacherRepository)
+    teachers = teacher_handle.handle()
+    return {"teachers": teachers.get_value()}
