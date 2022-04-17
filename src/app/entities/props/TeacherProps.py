8210f45f2913ce3e41b0adfd78fcf86e896e9657
Antonio-Gabriel <antoniocamposgabriel@gmail.com>
2022-04-17 16:24:04 +0100

    update teacher props

--- src/app/entities/props/TeacherProps.py
+++ src/app/entities/props/TeacherProps.py
@@ -5,5 +5,5 @@ from dataclasses import dataclass
 @dataclass
 class TeacherProps:
     user_id: str
-    created_at: datetime
-    updated_at: datetime
+    created_at: datetime = None
+    updated_at: datetime = None
