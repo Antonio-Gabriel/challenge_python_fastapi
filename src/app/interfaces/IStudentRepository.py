398c4d846cc5bdc53369227f13e3a4385aac907b
Antonio-Gabriel <antoniocamposgabriel@gmail.com>
2022-04-18 10:29:37 +0100

    student repository interface

---
+++ src/app/interfaces/IStudentRepository.py
@@ -0,0 +1,36 @@
+from typing import List
+from abc import ABC, abstractmethod
+
+from ..entities.props import StudentProps, ContactProps
+
+
+class IStudentRepository(ABC):
+    @abstractmethod
+    def save(entity: StudentProps):
+        """save entity into db"""
+
+        raise NotImplementedError("Method not implemented")
+
+    @abstractmethod
+    def register_contacts(*entity: List[ContactProps]):
+        """save contacts into db"""
+
+        raise NotImplementedError("Method not implemented")
+
+    @abstractmethod
+    def get():
+        """Get all teachers"""
+
+        raise NotImplementedError("Method not implemented")
+
+    @abstractmethod
+    def get_by_id(entity_id: str):
+        """get entity by id"""
+
+        raise NotImplementedError("Method not implemented")
+
+    @abstractmethod
+    def delete(entity_id: str):
+        """delete entity into db"""
+
+        raise NotImplementedError("Method not implemented")
