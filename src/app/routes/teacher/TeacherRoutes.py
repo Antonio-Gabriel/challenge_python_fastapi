195d128d7db657cd714ebbe2387da8eb3337b982
Antonio-Gabriel <antoniocamposgabriel@gmail.com>
2022-04-18 01:37:17 +0100

    update teacher routes

--- src/app/routes/teacher/TeacherRoutes.py
+++ src/app/routes/teacher/TeacherRoutes.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, status, Depends
 
+from ...adapter import UserAdapter
 from ...config import AuthMiddleware
 from ...decorators.Authorization import Authorization
 from ...repositories import UserRepository, TeacherRepository
@@ -8,6 +9,19 @@ from ...repositories import UserRepository, TeacherRepository
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 
+from .schemas.TeacherSchema import TeacherSchema
+from .schemas.TeacherResponseModel import TeacherResponseModel
+
+from ..user.schemas.UserResponseModel import GenericUserModel
+from ..user.schemas.AuthSchema import AuthSchema, AuthResponseModel
+
+from ...handlers import (
+    AuthUserHandler,
+    CreateTeacherHandler,
+    GetAllTeachersHandler,
+    DeleteTeacherHandler,
+)
+
 
 teacher_routes = APIRouter(
     prefix="/api",
@@ -15,7 +29,96 @@ teacher_routes = APIRouter(
 )
 
 
-# @teacher_routes.get("/teachers")
-# async def get_teachers():
+@teacher_routes.post("/teacher/auth", response_model=AuthResponseModel)
+async def auth(credentials: AuthSchema):
+    """Authenticate user"""
 
-#     return {"message": "Hello"}
+    auth_handle = AuthUserHandler(UserRepository)
+    response = auth_handle.handle(
+        email=credentials.email, password=credentials.password
+    )
+    error = response.error_value()
+
+    if error:
+        return JSONResponse(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            content=jsonable_encoder(
+                {
+                    "detail": error,
+                }
+            ),
+        )
+
+    return {
+        "access_token": AuthMiddleware.encode_token(
+            response.get_value().user_type.value
+        ),
+        "token_type": "Bearer",
+        "user": response.get_value(),
+    }
+
+
+@teacher_routes.get("/teachers", response_model=TeacherResponseModel)
+@Authorization("staff")
+async def get_teachers(auth=Depends(AuthMiddleware.auth_wrapper)):
+
+    teacher_handle = GetAllTeachersHandler(TeacherRepository)
+    teachers = teacher_handle.handle()
+    return {"teachers": teachers.get_value()}
+
+
+@teacher_routes.post(
+    "/teacher/create",
+    response_model=GenericUserModel,
+    response_description="Success Responde",
+)
+@Authorization("staff")
+async def create_teacher(
+    user_props: TeacherSchema, auth=Depends(AuthMiddleware.auth_wrapper)
+):
+    """Create user staff"""
+
+    user_result = UserAdapter.create(
+        name=user_props.name,
+        email=user_props.email,
+        surname=user_props.surname,
+        password=user_props.password,
+        city=user_props.city,
+        state=user_props.state,
+        user_type="teacher",
+    )
+
+    error_entity = user_result.error_value()
+    if error_entity:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content=jsonable_encoder(
+                {
+                    "detail": {"msg": error_entity},
+                }
+            ),
+        )
+
+    teacher_handler = CreateTeacherHandler(UserRepository, TeacherRepository)
+    result = teacher_handler.handle(user_result.get_value(), user_props.contacts)
+
+    error = result.error_value()
+
+    if error:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content=jsonable_encoder(
+                {
+                    "detail": {"msg": error},
+                }
+            ),
+        )
+
+    return JSONResponse(
+        status_code=status.HTTP_201_CREATED,
+        content=jsonable_encoder(
+            {
+                "detail": {"msg": "Successfully teacher created!"},
+            }
+        ),
+    )
